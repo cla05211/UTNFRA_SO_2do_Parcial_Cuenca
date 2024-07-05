eaa460a76626f9fbfebee347c451eaea44857649
Clara <cla05211@gmail.com>
2024-07-05 14:57:56 +0000

    Agrego a punto D

--- PuntoD.sh
+++ PuntoD.sh
@@ -5,3 +5,12 @@ cd estructura/
 cd tasks/
 vim /home/clara/.vimrc
 vim main.yml 
+vim datos_alumno.txt
+cd templates/
+vim datos_alumno.txt
+cd tasks/
+vim main.yml
+cd vars/
+vim main.yml 
+cd templates/
+vim datos_alumno.txt 
